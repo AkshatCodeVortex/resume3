7958ce8a8d1ab1920e8b8ba71740ff4c1292a0d5
Akshat Verma <96902325+AkshatCodeVortex@users.noreply.github.com>
2024-01-14 21:15:37 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+
+int n,a=0,b=1,c,i;
+printf("Enter the number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+    printf("%d",a);
+    c=a+b;
+    a=b;
+    b=c;
+}
+{
+
+return 0;
+}
+
+
+    
